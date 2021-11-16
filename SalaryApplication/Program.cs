27ddc96ca28902e9b8ffc04520b10bb54f6fedc0
Rauf84 <rauf@karimli.se>
2021-11-16 17:34:28 +0100

    Hello World

---
+++ SalaryApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalaryApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
